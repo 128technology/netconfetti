c3afa5f97ebd9f17a61d767195015759788a49a3
Dillon Buchanan <thedillonb@gmail.com>
2018-08-24 11:00:33 -0400

    Fix Default Export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { default, IClientOptions } from "./client";
+export { default as Client, IClientOptions } from "./client";
 export { default as RPCError } from "./rpcError";
 export { default as RPCErrors } from "./rpcErrors";
